5b322c9751f55381b43b4a4c1b8cb882e6a93cb3
Rosa Green <error404nullnotfound@disroot.org>
2024-11-28 12:24:13 -0600

    minor changes (2)

---
+++ rss-builder.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+. ./post-builder.sh
+post_list_replace() {
+  posts=""
+  for i in $1 
+  do
+    posts="$posts$(convert_post "posts/$i" rss/post-list-template.xml)"
+  done
+  echo "$posts"
+}
+replace '${POSTS}' "$(post_list_replace "$(cat post-list)")" "$(cat src/rss.xml)" > render/rss.xml
+replace '${TIME}' "$(TZ=GMT date +"%Y-%m-%dT%H:%M:%SZ")" "$(cat render/rss.xml)" > render/rss.xml
